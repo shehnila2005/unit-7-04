cf876c0b81cfb0650c14030179904c1619ef64ff
Shehnila Azmat <shehnila.azmat@ocsbstudent.ca>
2021-02-22 20:45:35 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let age = 0
+let day = 0
+
+document.getElementById('button').addEventListener ('click' , movie)
+function movie () {
+  age = document.getElementById('input').value 
+  day = document.getElementById('day').value
+  
+  
+  if ((day === 'tuesday' || day === 'thursday') || (age >= 12 && age <= 21)) { 
+      document.getElementById('answer').innerHTML = 'you can get it for free'
+  } else {
+      document.getElementById('answer').innerHTML = 'you need to pay'
+}
+}
